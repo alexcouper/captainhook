c0badeed7b9eecc5fb847bf4a4ea57304fe076a2
Daniel Watkins <daniel@daniel-watkins.co.uk>
2014-05-09 09:09:20 +0100

    Move python3 checker to its own file.

--- captainhook/pre_commit.py
+++ captainhook/pre_commit.py
@@ -26,30 +26,9 @@ except ImportError:
 from contextlib import contextmanager
 import os.path
 import sys
-from subprocess import Popen, PIPE
 
-
-class bash(object):
-    "This is lower class because it is intended to be used as a method."
-
-    def __init__(self, cmd):
-        """
-        TODO: Release this as a separate library!
-        """
-        self.p = None
-        self.output = None
-        self.bash(cmd)
-
-    def bash(self, cmd):
-        self.p = Popen(cmd, shell=True, stdout=PIPE, stdin=PIPE, stderr=PIPE)
-        self.output, self.err = self.p.communicate(input=self.output)
-        return self
-
-    def __str__(self):
-        return self.output.strip().decode(encoding='UTF-8')
-
-    def __nonzero__(self):
-        return bool(str(self))
+from captainhook.checkers.python3 import python3
+from captainhook.utils import bash, python_files_for_commit
 
 
 def title_print(msg):
@@ -58,28 +37,6 @@ def title_print(msg):
     print(bar)
     print(msg)
     print(bar)
-
-
-def python_files_for_commit():
-    "Get all python files that are staged for commit, that are not deleted."
-    files_pattern = '\.py(\..+)?$'
-    return bash((
-        "git diff --cached --name-status | "
-        "grep -E '{files_pattern}' | "
-        "grep -v -E '^D' | "
-        "awk '{{ print ( $(NF) ) }}' "
-    ).format(files_pattern=files_pattern))
-
-
-def python3():
-    "Check to see if python files are py3 compatible"
-    py_files = str(python_files_for_commit())
-    errors = []
-    for py_file in py_files.splitlines():
-        b = bash('python3 -m py_compile {0}'.format(py_file))
-        if b.err:
-            errors.append(b.err)
-    return "\n".join(errors)
 
 
 def pdb():
