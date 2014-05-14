a222df936f77284e8b9cc05c4caeecbef799bb88
Daniel Watkins <daniel@daniel-watkins.co.uk>
2014-05-14 11:14:04 +0100

    Use grep checker in pdb checker.

--- captainhook/checkers/grep.py
+++ captainhook/checkers/grep.py
@@ -7,8 +7,12 @@ DEFAULT = 'off'
 NO_ARG_MESSAGE = 'You must specify an argument to the grep checker.'
 
 
+def grep(args, files):
+    return bash('grep -n -H --exclude=tox.ini {} {}'.format(args, files))
+
+
 def run(arg=None):
     if arg is None:
         return NO_ARG_MESSAGE
     files = str(get_files_for_commit()).replace('\n', ' ')
-    return bash('grep -H --exclude=tox.ini {} {}'.format(arg, files))
+    return grep(arg, files)
