c1af09851c97a3413a4468025dc5edf9e95ab15d
Alex Couper <amcouper@gmail.com>
2014-06-02 23:49:21 +0000

    Add python feature test. Use py2to3-27.

--- features/steps/file_steps.py
+++ features/steps/file_steps.py
@@ -3,6 +3,12 @@ from behave import when
 
 @when('I create a file called "{file_name}" containing a pdb.set_trace()'
       ' statement')
-def step_impl(context, file_name):
+def step_pdb_set_trace(context, file_name):
     with open(file_name, 'w') as f:
         f.write("import pdb\npdb.set_trace()")
+
+
+@when(u'I create a file called "{file_name}" containing a print statment')
+def step_print_statement(context, file_name):
+    with open(file_name, 'w') as f:
+        f.write("print 'x'\n")
