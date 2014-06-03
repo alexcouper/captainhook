887275aa1a0d8f017508e3090097aa9db415d772
Alex Couper <amcouper@gmail.com>
2014-06-03 00:21:41 +0000

    Python3: Only check errors if we can't compile

--- captainhook/checkers/python3.py
+++ captainhook/checkers/python3.py
@@ -10,6 +10,10 @@ def run():
     "Check to see if python files are py3 compatible"
     errors = []
     for py_file in python_files_for_commit():
-        b = bash('2to3-2.7 {file}'.format(file=py_file))
-        errors.append(b.output.decode(encoding='UTF-8'))
+        # We only want to show errors if we CAN'T compile to py3.
+        # but we want to show all the errors at once.
+        b = bash('python3 -m py_compile {0}'.format(py_file))
+        if b.err:
+            b = bash('2to3-2.7 {file}'.format(file=py_file))
+            errors.append(b.output.decode(encoding='UTF-8'))
     return "\n".join(errors)
