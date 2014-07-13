b2ca51e810ee2165dd330611202c662bdd20b610
Alex Couper <amcouper@gmail.com>
2014-07-13 00:08:38 +0000

    Add block_branches checker.

---
+++ captainhook/checkers/block_branches.py
@@ -0,0 +1,27 @@
+# # # # # # # # # # # # # #
+# CAPTAINHOOK IDENTIFIER  #
+# # # # # # # # # # # # # #
+import argparse
+
+from .utils import bash
+
+CHECK_NAME = 'block_branch'
+
+
+def run(files, arg=None):
+    "Check we're not committing to a blocked branch"
+    parser = get_parser()
+    args = parser.parse_args(arg.split())
+
+    current_branch = bash('git symbolic-ref HEAD').output
+    current_branch = current_branch.replace('refs/heads/', '').strip()
+    if current_branch in args.branches:
+        return ("Branch '{0}' is blocked from being "
+                "committed to.".format(current_branch))
+
+
+def get_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('branches', metavar='B', nargs='+',
+                        help='a branch to block commits to')
+    return parser
