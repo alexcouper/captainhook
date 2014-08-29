ea2a1b8fcfccc74beb78c6230aa28fa1777c9aeb
Alex Couper <amcouper@gmail.com>
2014-08-29 22:30:23 +0000

    Fix the failing test.

--- captainhook/checkers/flake8_checker.py
+++ captainhook/checkers/flake8_checker.py
@@ -1,8 +1,10 @@
 # # # # # # # # # # # # # #
 # CAPTAINHOOK IDENTIFIER  #
 # # # # # # # # # # # # # #
+import os
 import sys
 from contextlib import contextmanager
+from os.path import join
 
 from .utils import filter_python_files
 
@@ -16,6 +18,7 @@ DEFAULT = 'on'
 CHECK_NAME = 'flake8'
 NO_FLAKE_MSG = ("flake8 is required for the flake8 plugin.\n"
                 "`pip install flake8` or turn it off in your tox.ini file.")
+REQUIRED_FILES = ['tox.ini']
 
 
 @contextmanager
@@ -35,12 +38,17 @@ def run(files, temp_folder):
     except ImportError:
         return NO_FLAKE_MSG
     from flake8.engine import get_style_guide
-    from flake8.main import DEFAULT_CONFIG
+
     py_files = filter_python_files(files)
     if not py_files:
         return
-    flake8_style = get_style_guide(config_file=DEFAULT_CONFIG, paths=['.'])
+    DEFAULT_CONFIG = join(temp_folder, 'tox.ini')
+
+    old_dir = os.curdir
+    os.chdir(temp_folder)
+    flake8_style = get_style_guide(config_file=DEFAULT_CONFIG)
     out, err = StringIO(), StringIO()
     with redirected(out, err):
         flake8_style.check_files(py_files)
+    os.chdir(old_dir)
     return out.getvalue().strip()
--- features/flake8_tox.feature
+++ features/flake8_tox.feature
@@ -3,7 +3,7 @@ Feature: flake8 obeys tox
     Background:
         Given that I am in a git repository
         And I have installed captainhook
-        And I have a flake8 section inside my tox.ini that excludes "a"
+        And I have a flake8 section inside my tox.ini that excludes "a/*"
 
     Scenario: When a flake8 section is present in tox.ini it should be obeyed.
         When I create a file in "a" called "ignore.py" containing a line over 80 chars
